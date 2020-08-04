5cc201894340ba4ed6b4d4b958fd16378de24519
ThibautB <dencis@MacBook-Pro-de-Dencis.local>
2020-08-04 21:21:40 +0200

    feat(<homePage>): create form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Url;
+use App\Form\UrlType;
+use DateTime;
 
 class HomeController extends AbstractController
 {
@@ -11,9 +15,18 @@ class HomeController extends AbstractController
      * 
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(Request $request)
     {
-        return $this->render('home.html.twig');
+        $url = new Url();
+        
+        $form = $this->createForm(UrlType::class, $url);
+        $url->setDateCreated(new DateTime('NOW'));
+        if($form->isSubmitted())
+        {
+            echo 'Hello';
+        }
+
+        return $this->render('home.html.twig', ['form' => $form->createView()]);
     }
 
 }
