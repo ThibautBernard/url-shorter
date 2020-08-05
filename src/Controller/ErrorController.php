bdb40314a8d431e1f2d9962929af1274caf87ed5
ThibautB <dencis@MacBook-Pro-de-Dencis.local>
2020-08-05 22:09:55 +0200

    style(<homepage><errorPage>): add div bootstrap

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -2,12 +2,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Url;
-use App\Form\UrlType;
-use DateTime;
-use App\Services\CorrectUrl;
 class ErrorController extends AbstractController
 {
 
@@ -15,9 +11,8 @@ class ErrorController extends AbstractController
      * 
      * @Route("/url/error", name="error")
      */
-    public function ErrorPage(Request $request)
+    public function errorPage()
     {
-       
         return $this->render('error.html.twig');
     }
 
